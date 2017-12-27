2525f0f6be133a597253d73ef79c70a5b4f9dddb
jk0629 <34863446+jk0629@users.noreply.github.com>
2017-12-27 18:47:37 +0800

    Main menu finish v1

---
+++ app/src/main/java/tw/edu/nutc/a70x9n0td/restaurantreservesystem/reg.java
@@ -0,0 +1,14 @@
+package tw.edu.nutc.a70x9n0td.restaurantreservesystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class reg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg);
+        setTitle("會員註冊"); //變更活動標題為會員註冊;
+    }
+}
