da896999543a58660f0667740cbb93e59797b68d
jk0629 <34863446+jk0629@users.noreply.github.com>
2017-12-27 11:10:29 +0800

    Initial commit

---
+++ app/src/main/java/tw/edu/nutc/a70x9n0td/restaurantreservesystem/MainMenu.java
@@ -0,0 +1,13 @@
+package tw.edu.nutc.a70x9n0td.restaurantreservesystem; //主畫面
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
---
+++ app/src/test/java/tw/edu/nutc/a70x9n0td/restaurantreservesystem/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package tw.edu.nutc.a70x9n0td.restaurantreservesystem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
